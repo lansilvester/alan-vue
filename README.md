803276e068317c28ce2d98c319be7006144d83d7
varland-lengkong <alan.lengkong@gmail.com>
2022-04-27 17:17:16 +0800

    Upload project vue

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# alan-vue
-Project aplikasi vue sebagai bahan belajar class --FULLSTACK LARAVEL VUE JS TOKO ONLINE--
+# alanvue
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderAlan />
+    <HeroAlan />
+    <WomanAlan />
+    <InstaAlan />
+    <PartnerAlan />
+    <FooterAlan />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderAlan from '@/components/HeaderAlan.vue';
+import HeroAlan from '@/components/HeroAlan.vue';
+import WomanAlan from '@/components/WomanAlan.vue';
+import InstaAlan from '@/components/InstaAlan.vue';
+import PartnerAlan from '@/components/PartnerAlan.vue';
+import FooterAlan from '@/components/FooterAlan.vue';
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderAlan,
+    HeroAlan,
+    WomanAlan,
+    InstaAlan,
+    PartnerAlan,
+    FooterAlan
+  }
+}
+</script>
